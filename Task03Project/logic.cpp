619b37f3559bf4648d5c4c4ce31d79cc588b08fd
polin-sv <polinasvetlay@icloud.com>
2026-04-06 17:12:59 +0300

    task03

--- Task03Project/logic.cpp
+++ Task03Project/logic.cpp
@@ -3,9 +3,16 @@
 // 
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму чисел от N до M.
+//best O(1)
+//worst O(N)
 
 #include "logic.h"
 
 int sum(int n, int m) {
-	return 0;
+	
+	if (n == -m) {
+		return 0;
+	}
+	
+	return m + sum(n, m - 1);
 }
