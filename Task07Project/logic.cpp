30dec77b97f8d19d8c15e8113fcef52d64cde7e3
polin-sv <polinasvetlay@icloud.com>
2026-04-06 18:31:10 +0300

    sof task07

--- Task07Project/logic.cpp
+++ Task07Project/logic.cpp
@@ -3,3 +3,17 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая считает количество нечётных цифр в заданном целом числе.
+
+#include "logic.h"
+
+int count_of_odd_digits(int number) {
+	number = number < 0 ? -number : number;
+
+	if (number ==0) {
+		return 0;
+	}
+	int d1 = number % 10;
+	int count = d1 % 2 == 0 ? 0 : 1;
+
+	return count + count_of_odd_digits(number / 10);
+}
