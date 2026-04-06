7ce01e2b24e898a972c705ae144ee0dedb386f7d
polin-sv <polinasvetlay@icloud.com>
2026-04-06 16:49:26 +0300

    soft task01

--- Task01Project/logic.cpp
+++ Task01Project/logic.cpp
@@ -3,3 +3,18 @@
 // 
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму чисел от 1 до N.
+
+int sum(int number) {
+	if (number < 0) {
+		return -1;
+	}
+	if (number == 0) {
+		return 0;
+	}
+
+	if (number == 1) {
+		return 1;
+	}
+
+	return number + sum(number - 1);
+}
