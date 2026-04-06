9a972510ab829607cc8a2c54897c77123cb597c8
polin-sv <polinasvetlay@icloud.com>
2026-04-06 18:15:30 +0300

    soft task06

--- Task06Project/logic.cpp
+++ Task06Project/logic.cpp
@@ -3,3 +3,29 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму чётных чисел от 1 до N.
+#include "logic.h"
+int recursion(int number) {
+
+	if (number == 2) {
+		return 2;
+	}
+
+	return number + recursion(number - 2);
+}
+
+
+int sum(int number) {
+	if (number < 0) {
+		return -1;
+	}
+
+	if (number < 2) {
+		return 0;
+	}
+
+	if (number % 2 == 1) {
+		number -= 1;
+	}
+	
+	return recursion(number);
+}
